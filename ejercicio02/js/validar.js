b5f86645d224b57b0844fd93af14504938b0b7f1
a22luisap <a22luisap@iessanclemente.net>
2023-06-11 09:35:39 +0200

    Ejercicios de páginas web dinámicas

--- ejercicio02/js/validar.js
+++ ejercicio02/js/validar.js
@@ -5,13 +5,13 @@ function cambiarImagen() {
   // Añadimos un event listener al hacer clic en la imagen
   imagen.addEventListener("click", function () {
     // Verificamos si la imagen actual es "circulo.png"
-    if (imagen.getAttribute("src") === "circulo.png") {
+    if (imagen.getAttribute("src") === "img/circulo.png") {
       // Si es así, cambiamos la fuente de la imagen a "cruz.png"
-      imagen.setAttribute("src", "cruz.png");
+      imagen.setAttribute("src", "img/cruz.png");
     } else {
       // Si no, la imagen actual es "cruz.png" o cualquier otra imagen,
       // por lo que cambiamos la fuente de la imagen a "circulo.png"
-      imagen.setAttribute("src", "circulo.png");
+      imagen.setAttribute("src", "img/circulo.png");
     }
   });
 }
